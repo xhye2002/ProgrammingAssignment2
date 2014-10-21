e4e1cf7d0747b00d9bfa69047b25c73d819fe4a7
alicia2006 <xhye2002@hotmail.com>
2014-10-21 08:40:50 -0700

    calcuate the inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Compute the inverse of a matrix and cache the result
+#If the content of the matrix are not changing, just look up
+#the inverse in the cache rather than recomputed
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "matrix": 
+## return a list of functions containing:
+## 1. function to set the matrix;
+## 2. function get the matrix; 
+## 3. function to set the inverse;
+## 4. function to get the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        
+        # set matrix
+        set <- function(y) {
+                x <<- y
+                inv<- NULL
+        }
+        # get matrix
+        get <- function() x
+        
+        #set inverse
+        setInverse <- function(inverse) inv <<- inverse
+        
+        #get inverse
+        getInverse <- function() inv
+        
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
-
+## calculate the inverse of the special "matrix" 
+## created by makeCacheMatrix function; 
+## if it is calculated before, return the cached inverse;
+## otherwise, calculate the inverse, save it to the cache 
+## and return the value
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+        
+        ## if it is calcuated already, return the cache
+        if (!is.null(inv)) {
+                message("getting cached inverse")
+                return(inv)
+        }
+  
+        ## caculate the inverse
+        data <- x$get()
+        inv <-solve(data, ...)
+        
+        ## cache the inverse
+        x$setInverse(inv)
+        
+        ## return the inverse
+        inv
 }
